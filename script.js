9b14b677cbe7296009270029f96645b53333652d
bykimus-prime <alexander.rodzyanko@gmail.com>
2022-07-24 18:28:15 +0900

    Adjust functions, add divide by 0 message

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-const add = function (num1, num2) {
-    return num1 + num2;
+
+
+// arithmetic functions
+function add(num1, num2) {
+    let addValue = (num1 + num2);
+    return addValue;
 };
 
-const subtract = function (num1, num2) {
-    return num1 - num2;
+function subtract(num1, num2) {
+    let subtractValue = (num1 - num2);
+    return subtractValue;
+};
+
+function multiply(num1, num2) {
+    let multiplyValue = (num1 * num2);
+    return multiplyValue;
+};
+
+function divide(num1, num2) {
+    let divideValue = (num1 / num2);
+
+    if (divideValue == "Infinity") {
+        return alert('Error! Can\'t divide by zero. You know the drill.');
+    } else return divideValue;
+}
+
+const power = function (num1, num2) {
+    return num1 ** num2;
 };
 
 const sum = function (array) {
     return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function (array) {
-    return array.reduce((num1, num2) => num1 * num2);
-};
+function operate() {
 
-const divide = function (array) {
-    return array.reduce((num1, num2) => num1 / num2);
 }
-
-const power = function (num1, num2) {
-    return num1 ** num2;
-};
