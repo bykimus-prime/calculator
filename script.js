594ec42ce1ab29cb9e075b16eb916a5bec24bf4d
bykimus-prime <alexander.rodzyanko@gmail.com>
2022-07-31 17:41:16 +0900

    Add ability to round numbers in the arithmetic functions to 2 places

--- script.js
+++ script.js
@@ -8,26 +8,34 @@
 // arithmetic functions
 function add(num1, num2) {
     let addValue = (num1 + num2);
+    let rounded = Math.round((addValue + Number.EPSILON) * 100) / 100;
+    rounded = rounded.toFixed(2);
     return addValue;
 };
 
 function subtract(num1, num2) {
     let subtractValue = (num1 - num2);
+    let rounded = Math.round((subtractValue + Number.EPSILON) * 100) / 100;
+    rounded = rounded.toFixed(2);
     return subtractValue;
 };
 
 function multiply(num1, num2) {
     let multiplyValue = (num1 * num2);
+    let rounded = Math.round((multiplyValue + Number.EPSILON) * 100) / 100;
+    rounded = rounded.toFixed(2);
     return multiplyValue;
 };
 
 function divide(num1, num2) {
     let divideValue = (num1 / num2);
+    let rounded = Math.round((divideValue + Number.EPSILON) * 100) / 100;
+    rounded = rounded.toFixed(2);
 
     if (divideValue == "Infinity") {
         return alert('Error! Can\'t divide by zero. You know the drill.');
     } else return divideValue;
-}
+};
 
 // const power = function (num1, num2) {
 //     return num1 ** num2;
