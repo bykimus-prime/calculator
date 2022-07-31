4fdebd45c494aaa3674c748e153c5b9cbebb8e1e
bykimus-prime <alexander.rodzyanko@gmail.com>
2022-07-31 21:48:07 +0900

    Create function to get values from clicking number buttons into arrays

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // global variables
 let num1 = 0;
 let num2 = 0;
+let tempNum1Array = [];
+let tempNum2Array = [];
+let savedFirstValue = 0;
 let operator = '';
 
 // arithmetic functions
@@ -8,6 +11,7 @@ function add(num1, num2) {
     let addValue = (num1 + num2);
     let roundedValue = Math.round((addValue + Number.EPSILON) * 100) / 100;
     roundedValue = roundedValue.toFixed(2);
+    console.log('add', rounded);
     const calcDisp = document.getElementById('displayArea');
     calcDisp.textContent = rounded;
 };
@@ -16,6 +20,7 @@ function subtract(num1, num2) {
     let subtractValue = (num1 - num2);
     let roundedValue = Math.round((subtractValue + Number.EPSILON) * 100) / 100;
     roundedValue = roundedValue.toFixed(2);
+    console.log('subtract', rounded);
     const calcDisp = document.getElementById('displayArea');
     calcDisp.textContent = rounded;
 };
@@ -24,6 +29,7 @@ function multiply(num1, num2) {
     let multiplyValue = (num1 * num2);
     let roundedValue = Math.round((multiplyValue + Number.EPSILON) * 100) / 100;
     roundedValue = roundedValue.toFixed(2);
+    console.log('multiply', rounded);
     const calcDisp = document.getElementById('displayArea');
     calcDisp.textContent = rounded;
 };
@@ -36,6 +42,7 @@ function divide(num1, num2) {
     } else {
         let roundedValue = Math.round((divideValue + Number.EPSILON) * 100) / 100;
         roundedValue = roundedValue.toFixed(2);
+        console.log('divide', rounded);
         const calcDisp = document.getElementById('displayArea');
         calcDisp.textContent = rounded;
     }
@@ -53,15 +60,19 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
     switch (operator) {
         case '+':
+            console.log('passing through switch +')
             add(num1, num2);
             break;
         case '-':
+            console.log('passing through switch -')
             subtract(num1, num2);
             break;
         case '*':
+            console.log('passing through switch *')
             multiply(num1, num2);
             break;
         case '/':
+            console.log('passing through switch /')
             divide(num1, num2);
             break;
         default:
@@ -69,4 +80,31 @@ function operate(operator, num1, num2) {
     }
 }
 
-operate(operator, num1, num2);
+function calcFactory() {
+    
+    function getNumberInput() {
+        const operandBtns = document.querySelectorAll('.operand');
+        operandBtns.forEach((button) => {
+            button.addEventListener('click', () => {
+                console.log('click for first number');
+                savedFirstValue = button.getAttribute('value');
+                console.log(savedFirstValue);
+                const calcDisp = document.getElementById('displayArea');
+
+                if (operator == '') {
+                    tempNum1Array.push(savedFirstValue);
+                    console.log('show the array1', tempNum1Array);
+                    num1 = tempNum1Array.join('');
+                    console.log('saved first value is: ', num1);
+                    calcDisp.textContent = num1;
+                } else {
+                    tempNum2Array.push(savedFirstValue);
+                    console.log('show the array2', tempNum2Array);
+                    num2 = tempNum2Array.join('');
+                    console.log('num2 is: ', num2);
+                    calcDisp.textContent = num2;
+                }
+            })
+        })
+    }
+}
